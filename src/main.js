42d54abe6ecec52758b65953b50e596c8499d450
Unlimited-Zone <82119883+Unlimited-Zone@users.noreply.github.com>
2021-04-23 18:25:11 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/*Archer
+Longyu Li
+Game: Archer
+Time used: 30 hours
+
+Display the time remaining (in seconds) on the screen (10)
+Implement mouse control for player movement and mouse click to fire (20)
+Implement a simultaneous two-player mode (30)
+Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+
+Discord: Tony Alegria helped me understand use delay in Phaser 3
+*/
 
 //game configuration
 let config = {

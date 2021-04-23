838ccb9bfef34c7e8a71b7f6afc56b50c6cc3422
UnlimitedZone <lli142@ucsc.edu>
2021-04-23 17:58:32 +0800

    speed!

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -10,7 +10,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
 
         this.isFiring = false;      //track rocket firing status
         this.moveSpeed = 2;
-        this.firingSpeed = 7;
+        this.firingSpeed = 5;
 
         this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
